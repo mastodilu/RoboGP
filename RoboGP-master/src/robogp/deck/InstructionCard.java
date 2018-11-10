49e4bff900fa49abbeac578a30c2a8fcde2308b7
mastodilu <matteo.dilucchio@edu.unito.it>
2018-11-10 18:27:56 +0100

    fix priorita e getters

--- RoboGP-master/src/robogp/deck/InstructionCard.java
+++ RoboGP-master/src/robogp/deck/InstructionCard.java
@@ -7,8 +7,9 @@ package robogp.deck;
 public class InstructionCard{
 
     private String tipo;//uturn backup move3 move2 move1 turnleft turnright
-    private int priorita_min;
-    private int priorita_max;
+    private int priorita_min; //usato per calcolare 'priorita'
+    private int priorita_max; //usato per calcolare 'priorita'
+    private int priorita; // priorita' della carta
     private String path_icona;
 
     public InstructionCard(String _tipo){
@@ -51,6 +52,7 @@ public class InstructionCard{
         this.priorita_min = 10;
         this.priorita_max = 60;
         this.path_icona = "";
+        this.priorita = randomWithRange(priorita_min, priorita_max);
     }
 
     private void createTurnLeft(){
@@ -58,13 +60,14 @@ public class InstructionCard{
         this.priorita_min = 70;
         this.priorita_max = 410;
         this.path_icona = "";
+        this.priorita = randomWithRange(priorita_min, priorita_max);
     }
 
     private void createTurnRight(){
         this.tipo = "turnright";
         this.priorita_min = 80;
         this.priorita_max = 420;
-        this.path_icona = "";
+        this.priorita = randomWithRange(priorita_min, priorita_max);
     }
 
     private void createBackUp(){
@@ -72,34 +75,58 @@ public class InstructionCard{
         this.priorita_min = 430;
         this.priorita_max = 480;
         this.path_icona = "";
+        this.priorita = randomWithRange(priorita_min, priorita_max);
     }
 
     private void createMove1(){
         this.tipo = "move1";
-        this.priorita_min = 400;
+        this.priorita_min = 490;
         this.priorita_max = 660;
         this.path_icona = "";
+        this.priorita = randomWithRange(priorita_min, priorita_max);
     }
 
     private void createMove2(){
         this.tipo = "move2";
-        this.priorita_min = 430;
-        this.priorita_max = 480;
+        this.priorita_min = 670;
+        this.priorita_max = 780;
         this.path_icona = "";
+        this.priorita = randomWithRange(priorita_min, priorita_max);
     }
 
     private void createMove3(){
         this.tipo = "move3";
-        this.priorita_min = 430;
-        this.priorita_max = 480;
+        this.priorita_min = 790;
+        this.priorita_max = 840;
         this.path_icona = "";
+        this.priorita = randomWithRange(priorita_min, priorita_max);
     }
 
     @Override
     public String toString(){
         String s = "[";
-        s += tipo + "]";
+        s += tipo + ":" + getPriorita() + "]";
         return s;
     }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getPath_icona() {
+        return path_icona;
+    }
+
+    public int getPriorita() {
+        return priorita;
+    }
+    
+    
+    private int randomWithRange(int min, int max)
+    {
+       int range = (max - min) + 1;     
+       return (int)(Math.random() * range) + min;
+    }
+    
 
 }
