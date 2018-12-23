8eb29f7e0776a699a8350d7c6cae14632bfd5198
st148550 <di_lucchio.matteo@educ.di.unito.it>
2018-12-23 15:47:38 +0100

    handle direction with Direction class

--- RoboGP-master/src/robogp/Giocatore/Robot/RobotMarkerPlaying.java
+++ RoboGP-master/src/robogp/Giocatore/Robot/RobotMarkerPlaying.java
@@ -1,16 +1,17 @@
 package robogp.Giocatore.Robot;
 
 import robogp.matchmanager.RobotMarker;
+import robogp.robodrome.Direction;
 
 public class RobotMarkerPlaying extends RobotMarker{
 //per sapere la posizione del robot (e anche quella iniziale) guardiamo 'dockNumber' della super classe invece di startX-Y
-private final char startDirection; //direzione iniziale
+private final Direction startDirection; //direzione iniziale
 private final int saluteMax;
 private boolean spento;
 private int contatoreDanni;
 
     public RobotMarkerPlaying(
-                    int startDock, char startDirection,
+                    int startDock, Direction startDirection,
                     int saluteMax, int vite,
                     String name, String color, String owner ){
         super(name, color); // usiamo il costruttore che ci hanno dato
@@ -24,7 +25,7 @@ private int contatoreDanni;
         this.setVite(vite);
     }
 
-    public char getStartDirection() {
+    public Direction getStartDirection() {
         return startDirection;
     }
 
