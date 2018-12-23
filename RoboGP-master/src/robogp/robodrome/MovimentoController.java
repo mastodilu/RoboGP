3333e302b9b3e645e417932176a40d3459ddef14
st148550 <di_lucchio.matteo@educ.di.unito.it>
2018-12-23 21:45:21 +0100

    cambiato completamente il modo di usare le variabili della classe

--- RoboGP-master/src/robogp/robodrome/MovimentoController.java
+++ RoboGP-master/src/robogp/robodrome/MovimentoController.java
@@ -14,26 +14,60 @@ public class MovimentoController {
      * Istanza della view del robodromo.
      */
     RobodromeView rv;
+
     /**
      * Istanza del robodromo.
      */
     Robodrome rb;
+
     /**
      * Istanza del robot.
      */
     RobotMarker rm;
+
     /**
      * Istanza della scheda istruzione.
      */
     InstructionCard ic;
+
+    /**
+     * Riga corrente del robot.
+     */
+    int rigaCorrente;
+ 
+    /**
+     * Colonna corrente del robot.
+     */
+    int colonnaCorrente;
+    
+    /**
+     * Direzione corrente del robot.
+     */
+    Direction direzioneCorrente;
+    
+    /**
+     * Cella corrente del robot.
+     */
+    BoardCell cellaCorrente;
     
     /**
      * Costruttore parametrico.
      * @param rb istanza del robodromo.
      */
     public MovimentoController(RobodromeView rv){
-        this.rv = rv;
-        this.rb = rv.getDrome();
+        this.rv = rv; // RobodromeView
+        this.rb = rv.getDrome(); // Robodrome
+    }
+    
+    /**
+     * Inizializza le variabili coi valori correnti del robot
+     * passato come parametro.
+     */
+    private void initVariabili(){
+        rigaCorrente = this.rm.getRiga();
+        colonnaCorrente = this.rm.getColonna();
+        cellaCorrente = this.rb.getCell(rigaCorrente, colonnaCorrente);
+        direzioneCorrente = this.rm.getCurrentDirection();
     }
     
     /**
@@ -41,37 +75,45 @@ public class MovimentoController {
      * @param ic la scheda istruzione dal quale leggere la mossa
      * @param rm il robot da muovere
      */
-    public void addRobotMove(InstructionCard ic, RobotMarker rm){
+    public RobotMarker muoviRobot(InstructionCard ic, RobotMarker rm){
         this.ic = ic;
         this.rm = rm;
+        initVariabili();
         
-        int movimento = ic.getMovimento();
-        int passi = 0;//conta quanti passi puo' effettuare il robot
+        int movimento = ic.getMovimento();//di quanto dovrebbe muoversi
+        int passiEffettuati;
         Rotation rotazione = ic.getRotazione();
-        boolean backup = ic.getTipo().equals("backup");//indietro di un passo
-        BoardCell iniziale, finale;
-            iniziale = rb.getCell(rm.getRiga(), rm.getColonna());
+        boolean backup = ic.getTipo().equals("backup");//se ic e' backup card deve gestire il movimento all'indietro
+        BoardCell cellaSuccessiva;
+        
+        cellaCorrente.robotOutside();
         
         //inverte MOMENTANEAMENTE la direzione del robot in modo da farlo camminare in avanti
         //per sfruttare il metodo 'movimentoAmmissibile()'
-        if(backup)  this.rm.setDirection(this.direzioneOpposta(this.rm.getDirection()));
+        if(backup)  direzioneCorrente = direzioneOpposta(direzioneCorrente);
         
         //movimentoAmmissibile controlla di poter avanzare di una casella in avanti
         while(movimento > 0 && movimentoAmmissibile()){
             movimento--;
-            passi++;
+            cellaSuccessiva = this.cellaSuccessiva(rigaCorrente, colonnaCorrente, direzioneCorrente);
+            this.rigaCorrente = cellaSuccessiva.getRiga();
+            this.colonnaCorrente = cellaSuccessiva.getColonna();
         }
         
         //ripristina la direzione originale del robot
-        if(backup)  this.rm.setDirection(this.direzioneOpposta(this.rm.getDirection()));
+        if(backup)  direzioneCorrente = direzioneOpposta(direzioneCorrente);
+        
+        passiEffettuati = ic.getMovimento() - movimento;
+        
+        aggiornaVariabiliRobot();
         
         //esegue l'animazione del movimento
-        this.rv.addRobotMove(rm, passi, rm.getDirection(), rotazione);
+        this.rv.addRobotMove(rm, passiEffettuati, direzioneCorrente, rotazione);
         
-        //calcola la cella raggiunta dopo n passi
-        finale = cellaFinale(iniziale, rm.getDirection(), passi);
+        System.out.println("Mosso di " + passiEffettuati + " in avanti verso "
+                + direzioneCorrente);
         
-        aggiornaVariabili(finale, rm.getDirection());
+        return this.rm;
     }
     
     
@@ -80,19 +122,16 @@ public class MovimentoController {
      * @param finale la cella finale raggiunta dal robot
      * @param direzioneFinale
      */
-    private void aggiornaVariabili( BoardCell finale,
-                                             Direction direzioneFinale){
-        int rigaFinale = finale.getRiga();
-        int colonnaFinale = finale.getColonna();
-        
+    private void aggiornaVariabiliRobot(){
         //robot
-        rm.setPosizione(rigaFinale, colonnaFinale);
-        rm.setDirection(direzioneFinale);
-        rm.updateStoricoPosizioni(rigaFinale, colonnaFinale);
-        rm.updateStoricoDirezioni(direzioneFinale);
+        System.out.println("Posizione finale " + rigaCorrente + " " + colonnaCorrente);
+        rm.setPosizione(rigaCorrente, colonnaCorrente);
+        rm.setDirection(direzioneCorrente);
+        rm.updateStoricoPosizioni(rigaCorrente, colonnaCorrente);
+        rm.updateStoricoDirezioni(direzioneCorrente);
         
         //cella
-        finale.setRobot();
+        this.rb.getCell(rigaCorrente, colonnaCorrente).robotInside();
     }
     
     
@@ -101,9 +140,29 @@ public class MovimentoController {
      * @return true se non ci sono ostacoli, false altrimenti.
      */
     public boolean movimentoAmmissibile(){
+        if(bloccatoDaBordi())           return false;
         if(bloccatoDaMuri())            return false;
         if(robotInCellaSuccessiva())    return false;
         return true;
+    }
+    
+    
+    /**
+     * Controlla che il movimento rispetto alla posizione corrente
+     * non sia bloccato da bordi.
+     * @return true se gli indici escono dalla mappa, false altrimenti.
+     */
+    private boolean bloccatoDaBordi(){
+        if(direzioneCorrente == Direction.W && colonnaCorrente == 0)
+            return true;
+        else if(direzioneCorrente == Direction.N && rigaCorrente == 0)
+            return true;
+        else if(direzioneCorrente == Direction.E && colonnaCorrente == rb.getColumnCount()-1)
+            return true;
+        else if(direzioneCorrente == Direction.S && rigaCorrente == rb.getRowCount()-1)
+            return true;
+        
+        return false;
     }
 
 
@@ -114,17 +173,12 @@ public class MovimentoController {
      *      false se il passaggio e' libero e ci si puo' muovere
      */
     private boolean bloccatoDaMuri() {
-        Direction d = this.rm.getDirection();
-        Direction opposta = direzioneOpposta(d);
-        int riga, colonna; //riga e colonna correnti del robot
-            riga = this.rm.getRiga();
-            colonna = this.rm.getColonna();
-        BoardCell corrente, successiva;
-            corrente = rb.getCell(riga, colonna);
-            successiva = cellaSuccessiva(riga, colonna, d);
+        Direction opposta = direzioneOpposta(direzioneCorrente);
+
+        BoardCell successiva = cellaSuccessiva(rigaCorrente, colonnaCorrente, direzioneCorrente);
         
         //controlla muro in questa cella
-        if(corrente.hasWall(d))     return true;
+        if(cellaCorrente.hasWall(direzioneCorrente))     return true;
         
         //controlla muri nella cella successiva
         if(successiva == null || successiva.hasWall(opposta))
@@ -193,14 +247,24 @@ public class MovimentoController {
      *      false altrimenti
      */
     private boolean robotInCellaSuccessiva() {
-        Direction d = this.rm.getDirection();
-        int riga, colonna; //riga e colonna correnti del robot
-            riga = this.rm.getRiga();
-            colonna = this.rm.getColonna();
-        BoardCell successiva;
-            successiva = cellaSuccessiva(riga, colonna, d);
-        
-        return successiva.hasRobot();
+        return cellaSuccessiva(rigaCorrente, colonnaCorrente, direzioneCorrente).hasRobot();
+    }
+    
+    
+    
+    /**
+     * Piazza il robot in una posizione iniziale specificata a mano.
+     * @param rm robot
+     * @param d direzione
+     * @param r riga
+     * @param c colonna
+     */
+    public void placeRobot(RobotMarker rm, Direction d, int r, int c){
+        rv.placeRobot(rm, d, r, c, true); // aggiunge il robot al tabellone        
+        rm.setDirection(d);
+        rm.setPosizione(r, c);
+        rm.updateStoricoDirezioni(d);
+        rm.updateStoricoPosizioni(r, c);
     }
 
 }
