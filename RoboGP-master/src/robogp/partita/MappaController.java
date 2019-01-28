f82a72fd2fd0dcaa2d5b572a6cc438a53d2a889e
st148550 <matteo.dilucchio@edu.unito.it>
2019-01-28 16:01:36 +0100

    creata la classe MappaController

---
+++ RoboGP-master/src/robogp/partita/MappaController.java
@@ -0,0 +1,76 @@
+package robogp.partita;
+
+import java.util.ArrayList;
+import robogp.matchmanager.RobotMarker;
+import robogp.robodrome.Robodrome;
+import robogp.robodrome.view.RobodromeView;
+
+
+/**
+ *
+ * @author Matteo Di Lucchio <matteo.dilucchio@edu.unito.it>
+ */
+public class MappaController {
+    
+    /**
+     * Array di robot in partita.
+     */
+    private ArrayList<RobotMarker> robots;
+    
+    /**
+     * View del robodromo.
+     */
+    private RobodromeView rv;
+    
+    /**
+     * Robodromo della partita.
+     */
+    private Robodrome robodrome;
+    
+    /**
+     * Pattern singleton.
+     */
+    public static MappaController singleInstance;
+    
+    
+    /**
+     * Inizializza l'istanza di MappaController.
+     * @param r robodromo
+     * @param rv view del robodromo
+     * @param robots arraylist di robot in partita
+     */
+    public void init(Robodrome r, RobodromeView rv, ArrayList<RobotMarker> robots){
+        this.robodrome = r;
+        this.rv = rv;
+        this.robots = robots;
+    }
+    
+    
+    
+    /**
+     * Controlla che l'istanza sia inizializzata e pronta per essere usata.
+     * @return true se ogni variabile e' inizializzata, false altrimenti.
+     */
+    public boolean checkInit(){
+        return this.robodrome != null
+                && this.rv != null
+                && this.robots != null;
+    }
+    
+    /**
+     * Costruttore
+     */
+    private MappaController(){
+    }
+    
+    
+    /**
+     * @return l'istanza di mappa controller.
+     */
+    public static MappaController getInstance(){
+        if(singleInstance == null)
+            singleInstance = new MappaController();
+        return singleInstance;
+    }
+    
+}
